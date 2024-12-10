ce4a4cad9fc55d7fba16b9779da348a843123e6a
Caio Eduardo De Souza <127906616+Caiodusouza@users.noreply.github.com>
2024-12-09 22:31:41 -0300

    Add files via upload
    jogo simples de física com React, onde o jogador controla a inclinação de uma rampa para observar como a gravidade e a aceleração afetam o movimento de uma bola. O objetivo era centralizar os conceitos de física, como aceleração devido à gravidade e a influência do ângulo da rampa sobre o movimento.

---
+++ src/reportWebVitals.js
@@ -0,0 +1,10 @@
+// src/reportWebVitals.js
+
+// Remover ou comentar a linha abaixo
+// import { reportWebVitals } from 'web-vitals';
+
+const reportWebVitals = () => {
+  // Seu código aqui
+};
+
+export default reportWebVitals;
